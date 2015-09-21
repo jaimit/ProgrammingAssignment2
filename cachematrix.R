49140c6f017d941d5d7bcfa4cb18538476da4c63
jaimit <jpari004@fiu.edu>
2015-09-21 00:16:44 -0400

    Cachematrix file updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-    invmatrix <- NULL
+    invmatrix <- NULL  ## invmatrix variable caches the inverse of a matirx
+    
+    ## function set is used to update the value of the input matrix that needs to be inversed
     set <- function(y) {
-        x <<- y
-        invmatrix <<- NULL
+        x <<- y  
+        invmatrix <<- NULL ## in case the set function is called the invmatrix is set back to null
     }
+    
+    ## function get is used to get the value of the input matirx to the makeCacheMatrix function
     get <- function() x
+    
+    ## function setinverse is used to set the value of the invmatrix obtained for caching later
     setinverse <- function(invm) invmatrix <<- invm
+    
+    ## function getinverse is used to get the value of the invmatix cached in the makeCacheMatrix
     getinverse <- function() invmatrix
+    
+    ## passing all the 4 functions as a output of the makeCacheMatrix 
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -19,15 +26,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     invmatrix1 <- x1$getinverse()
+    ## if the inversematrix in cached return its value
     if(!is.null(invmatrix1)) {
         message("getting cached data")
         return(invmatrix1)
     }
+    ## if the inversematrix is not chached yet get the matrix and find its inverse
     data <- x1$get()
     invmatrix1 <- solve(data, ...)
     x1$setinverse(invmatrix1)
